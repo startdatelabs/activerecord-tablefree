a26a2b5ead7193a06d2b801d45d1ecfbb93c1af0
behrooz shabani (everplays) <everplays@gmail.com>
2018-08-12 19:54:15 +0200

    ability to use database specific columns
    First of all, `sql_type` is delegated to cast_class in
    `ActiveRecord::ConnectionAdapters::Column` which also means that the
    order of passed parameters were wrong.
    
    Aside from that, this changeset allows to use database specific types
    that are not necessarily under `ActiveRecord::Type`. For example, this
    is how one could use Postgres' array:
    
    ```ruby
    class FooBar < ApplicationRecord
      has_no_table
    
      column :an_array_of_integers,
        ActiveRecord::ConnectionAdapters::PostgreSQL::OID::Array,
        nil,
        true,
        [ActiveRecord::Type::Integer.new]
    end
    ```

--- lib/activerecord/tablefree.rb
+++ lib/activerecord/tablefree.rb
@@ -86,11 +86,15 @@ module ActiveRecord
       end
 
       # Register a new column.
-      def column(name, sql_type = nil, default = nil, null = true)
-        cast_class = "ActiveRecord::Type::#{sql_type.to_s.camelize}".constantize rescue nil
+      def column(name, sql_type = nil, default = nil, null = true, cast_class_params = nil)
+        cast_class = if sql_type.is_a?(Class) && sql_type < ActiveModel::Type::Value
+                       sql_type
+                     else
+                       "ActiveRecord::Type::#{sql_type.to_s.camelize}".constantize rescue nil
+                     end
         raise InvalidColumnType, "sql_type is #{sql_type} (#{sql_type.class}), which is not supported" unless cast_class.respond_to?(:new)
-        cast_type = cast_class.new
-        tablefree_options[:columns_hash][name.to_s] = ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default, cast_type, sql_type.to_s, null)
+        cast_type = cast_class.new(*cast_class_params)
+        tablefree_options[:columns_hash][name.to_s] = ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default, cast_type, null)
       end
 
       # Register a set of columns with the same SQL type
